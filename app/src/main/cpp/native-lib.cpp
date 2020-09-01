a7dac85cd41e878ea0f472d55dc236c20b8c23d9
panyong <1062337192@qq.com>
2020-09-01 18:30:46 +0800

    1、添加md5算法 2、 实现java ，jni层byte数组参数相互传递

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,54 @@
 #include <jni.h>
 #include <string>
 #include "/openssl/crypto.h"
+#include "/openssl/md5.h"
+#include "/openssl/aes.h"
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring JNICALL
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"JNI",__VA_ARGS__)
+extern "C" JNIEXPORT jbyteArray JNICALL
 Java_com_example_jin_ende_1test_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
+        jobject /* this */, jbyteArray _jbyte) {
+   // std::string hello = "1234567891234567";
 //    return env->NewStringUTF(hello.c_str());
-    return env->NewStringUTF(OpenSSL_version(OPENSSL_VERSION));
+    //jboolean isCopy;
+   // jbyte *dataArray = env->GetByteArrayElements(_jbyte, 0);
+    //jsize dataLen = env->GetArrayLength(_jbyte);
+    // jbyte * arrayBody = env->GetByteArrayElements(data,0);
+
+
+    char *charPtr;
+    jsize dataLen = env->GetArrayLength(_jbyte);
+    jbyte *bytePtr = env->GetByteArrayElements(_jbyte, JNI_FALSE);
+    if (dataLen > 0) {
+        charPtr = (char *) malloc(dataLen + 1); //"\0"
+        memcpy(charPtr, bytePtr, dataLen);
+        charPtr[dataLen] = 0;
+        std::string st1 = charPtr;
+        LOGE("chris1 ：%s", st1.c_str());
+    }
+
+    MD5_CTX ctx;//context
+    //char *idata = const_cast<char *>(hello.c_str());
+    unsigned char md5[16];
+    memset(&ctx, 0, sizeof(ctx));
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, charPtr, dataLen);
+    MD5_Final(md5, &ctx);
+    int i;
+    for (i = 0; i < dataLen; i++) {
+       // LOGE("chris ：%02x", md5[i]);
+    }
+    //std::string str = (char *) md5;
+   // LOGE("chris :%s", str.c_str());
+
+    env->ReleaseByteArrayElements(_jbyte, bytePtr, 0);
+
+    // 转为baidu byte[] 返回zhidao
+    int length = sizeof(md5) / sizeof(char);
+    jbyteArray byteArray = env->NewByteArray(length);
+    env->SetByteArrayRegion(byteArray, 0, length, (jbyte *) md5);
+
+    return byteArray;
 }
--- app/src/main/java/com/example/jin/ende_test/MainActivity.java
+++ app/src/main/java/com/example/jin/ende_test/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.jin.ende_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        String str = "first blood";
+        String s;
+        try {
+            byte[] bytes = stringFromJNI(str.getBytes());
+            s = HexTest.byteArrToHex(bytes);
+            Log.e("chris jni", s);
+            tv.setText(s);
+            MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+            byte[] btArr = md5Digest.digest(str.getBytes());
+            s = HexTest.byteArrToHex(btArr);
+            Log.e("chris java", s);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //System.arraycopy();
+        //Arrays.CopyOf();
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    native byte[] stringFromJNI(byte[] clearText);
 }
